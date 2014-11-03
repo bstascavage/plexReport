91618aaf524547279c7e17184a08260231f50c6b
bstascavage <brian@stascavage.com>
2014-11-03 16:41:49 -0500

    Code commenting and other cleanup

--- bin/plexReport.rb
+++ bin/plexReport.rb
@@ -13,7 +13,7 @@ require_relative 'thetvdb'
 # Author: Brian Stascavage
 # Email: brian@stascavage.com
 #
-class ReadLog
+class PlexReport
     def initialize
         begin
             $config = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), '../etc/config.yaml') )
@@ -21,19 +21,24 @@ class ReadLog
             abort('Configuration file not found.  Exiting...')
         end
     end
-      
+     
+    # Method for retrieving information for all movies for a given period of time. 
     def getMovies
-	moviedb = TheMovieDB.new
-	plex = Plex.new
-	library = plex.get('/library/recentlyAdded')
-	movies = Array.new
+    	moviedb = TheMovieDB.new
+	    plex = Plex.new
+	    library = plex.get('/library/recentlyAdded')
+	    movies = Array.new
 
-	library['MediaContainer']['Video'].each do | element |
-	    if (Time.now.to_i - element['updatedAt'].to_i < 604800)
-		plex_movie = plex.get("/library/metadata/#{element['ratingKey']}")['MediaContainer']['Video']
-    	        movie_id = plex.get("/library/metadata/#{element['ratingKey']}")['MediaContainer']['Video']['guid'].gsub(/com.plexapp.agents.themoviedb:\/\//, '').gsub(/\?lang=en/, '')
+	    library['MediaContainer']['Video'].each do | element |
+	        if (Time.now.to_i - element['updatedAt'].to_i < 604800)
+		        plex_movie = plex.get("/library/metadata/#{element['ratingKey']}")['MediaContainer']['Video']
+    	        
+                # This is some contrivulted logic to strip off the moviedb.org id
+                # from the Plex mediadata.  I wish Plex made this information
+                # easier to get
+                movie_id = plex.get("/library/metadata/#{element['ratingKey']}")['MediaContainer']['Video']['guid'].gsub(/com.plexapp.agents.themoviedb:\/\//, '').gsub(/\?lang=en/, '')
     	        if !movie_id.include?('local') 
-	            begin
+	                begin
        	                movie = moviedb.get("movie/#{movie_id}")
 	                    movies.push({ 
 	                        :id       => movie['id'],
@@ -53,6 +58,8 @@ class ReadLog
 	    return movies.sort_by { |hsh| hsh[:title] }
     end
 
+    # Method for getting new TV episodes for a given time period.  
+    # This only returns new episodes, not seasons
     def getNewTVEpisodes
         thetvdb = TheTVDB.new
         plex = Plex.new
@@ -66,10 +73,19 @@ class ReadLog
                         !element.has_key?('updatedAt')))
 
                     show_episodes = plex.get("/library/metadata/#{element['parentRatingKey']}/allLeaves")
+
+                    # This checks for any shows that are exact one episode large.
+                    # This shows don't have an "updatedAt" field yet.
                     if show_episodes['MediaContainer']['size'].to_i == 1
                         begin
                             episode = show_episodes['MediaContainer']['Video']
+
+                            # This is some contrivulted logic to strip off the 
+                            # thetvdc.com id
+                            # from the Plex mediadata.  I wish Plex made this 
+                            # information easier to get
                             show_id = plex.get(episode['parentKey'])['MediaContainer']['Directory']['guid'].gsub(/.*:\/\//, '').gsub(/\/.*/, '')
+                            
                             show = thetvdb.get("series/#{show_id}/all/")['Data']['Series']
                             tv_episodes.push({
                                 :id             => show_id,
@@ -89,7 +105,12 @@ class ReadLog
                         show_episodes['MediaContainer']['Video'].each do | episode |
                             begin
                                 if (Time.now.to_i - episode['addedAt'].to_i < 604800) 
+                                    # This is some contrivulted logic to strip off 
+                                    # the thetvdc.com id
+                                    # from the Plex mediadata.  I wish Plex made this
+                                    # information easier to get
                                     show_id = plex.get(episode['parentKey'])['MediaContainer']['Directory']['guid'].gsub(/.*:\/\//, '').gsub(/\/.*/, '')
+
                                     show = thetvdb.get("series/#{show_id}/all/")['Data']['Series']
 
                                     tv_episodes.push({
@@ -112,9 +133,13 @@ class ReadLog
                 end
             end
         end 
-	return tv_episodes.sort_by { |hsh| hsh[:series_name] }  
+	    return tv_episodes.sort_by { |hsh| hsh[:series_name] }  
     end
 
+    # Method that returns the metadata for any new seasons added to the Plex.
+    # A season is determined by counting all of the "leaves" in Plex and
+    # checking that number against the total episodes in a season, according
+    # to thetvdb.com
     def getNewTVSeasons
         thetvdb = TheTVDB.new
         plex = Plex.new
@@ -123,6 +148,11 @@ class ReadLog
 
         library['MediaContainer']['Directory'].each do | element |
             if ((Time.now.to_i - element['addedAt'].to_i < 604800))
+
+                # This is some contrivulted logic to strip off the
+                # thetvdc.com id
+                # from the Plex mediadata.  I wish Plex made this
+                # information easier to get
                 show_id = plex.get(element['parentKey'])['MediaContainer']['Directory']['guid'].gsub(/.*:\/\//, '').gsub(/\/.*/, '').gsub(/\?.*/, '')
                 begin
                     show = thetvdb.get("series/#{show_id}/all/")['Data']
@@ -158,12 +188,16 @@ class ReadLog
     end
 end
 
-test = ReadLog.new
-new_episodes = test.getNewTVEpisodes
-new_seasons = test.getNewTVSeasons
-movies = test.getMovies
+# Main method that starts the report
+def main
+    test = PlexReport.new
+    new_episodes = test.getNewTVEpisodes
+    new_seasons = test.getNewTVSeasons
+    movies = test.getMovies
 
-#pp new_episodes
-YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), '../etc/config.yaml') )
-template = ERB.new File.new(File.join(File.expand_path(File.dirname(__FILE__)), "../etc/email_body.erb") ).read, nil, "%"
-puts template.result(binding)
+    YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), '../etc/config.yaml') )
+    template = ERB.new File.new(File.join(File.expand_path(File.dirname(__FILE__)), "../etc/email_body.erb") ).read, nil, "%"
+    puts template.result(binding)
+end
+
+main()
