5cf4b4f707a3adb46306218d37b039fb3e03a312
bstascavage <brian@stascavage.com>
2014-12-22 14:34:15 -0500

    Fixed issue where thetvdbdvdorderer was accidently added.

--- bin/plexReport.rb
+++ bin/plexReport.rb
@@ -208,7 +208,7 @@ class PlexReport
             # This is some contrivulted logic to strip off the moviedb.org id
             # from the Plex mediadata.  I wish Plex made this information
             # easier to get
-            if plex.get("/library/metadata/#{tv_show['ratingKey']}")['MediaContainer']['Directory']['guid'].include?("thetvdb")
+            if plex.get("/library/metadata/#{tv_show['ratingKey']}")['MediaContainer']['Directory']['guid'].include?("thetvdb:")
                 show_id = plex.get("/library/metadata/#{tv_show['ratingKey']}")['MediaContainer']['Directory']['guid'].gsub(/com.plexapp.agents.thetvdb:\/\//, '').gsub(/\?lang.*/, '')
             else
                 $logger.error("TV Show #{tv_show['title']} using incompatiable agent")
