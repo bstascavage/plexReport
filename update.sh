8c36ccb6d3f4788e65b5035523d3efc25de23f2e
bstascavage <brian@stascavage.com>
2015-02-24 10:45:00 -0500

    Added update script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Bash script to pull the newest code from Github and copy it to the correct locations
+PLEX_REPORT_LIB='/var/lib/plexReport'
+PLEX_REPORT_CONF='/etc/plexReport'
+
+/bin/echo "Grabbing newest code from https://github.com/bstascavage/plexReport"
+/usr/bin/git pull
+
+/bin/echo "Moving plexreport and plexreport-setup to /usr/local/sbin"
+/bin/cp bin/* /usr/local/sbin
+/bin/echo "Moving plexreport libraries to /var/lib/plexReport"
+/bin/cp lib/* $PLEX_REPORT_LIB
+/bin/echo "Moving email_body.erb to /etc/plexReport"
+/bin/cp etc/* $PLEX_REPORT_CONF
+
+/bin/echo "Installing ruby gem dependency"
+/use/bin/gem install bundler
+/usr/local/bin/bundle install
+
+/bin/echo "Upgrade complete"
