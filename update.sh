61cf66fc634691a4ecbd391dad273d468cd75f1c
bstascavage <brian@stascavage.com>
2015-03-16 10:49:43 -0400

    Fixed some recursive folder issues with the upgrade script

--- update.sh
+++ update.sh
@@ -7,14 +7,14 @@ PLEX_REPORT_CONF='/etc/plexReport'
 /usr/bin/git pull
 
 /bin/echo "Moving plexreport and plexreport-setup to /usr/local/sbin"
-/bin/cp bin/* /usr/local/sbin
+/bin/cp -r bin/* /usr/local/sbin
 /bin/echo "Moving plexreport libraries to /var/lib/plexReport"
-/bin/cp lib/* $PLEX_REPORT_LIB
+/bin/cp -r lib/* $PLEX_REPORT_LIB
 /bin/echo "Moving email_body.erb to /etc/plexReport"
-/bin/cp etc/* $PLEX_REPORT_CONF
+/bin/cp -r etc/* $PLEX_REPORT_CONF
 
 /bin/echo "Installing ruby gem dependency"
-/use/bin/gem install bundler
+/usr/bin/gem install bundler
 /usr/local/bin/bundle install
 
 /bin/echo "Upgrade complete"
